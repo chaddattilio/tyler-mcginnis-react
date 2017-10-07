453c2d483c8a71d4f5d39be62a22d22b9bf86d2e
Chad Dattilio <chaddattilio@gmail.com>
2017-10-07 16:11:18 -0400

    Updates to webpack / npm scripts / added to index.js

--- app/index.js
+++ app/index.js
@@ -3,5 +3,16 @@ var ReactDOM = require('react-dom');
 require('./index.css');
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div>
+        Hello React!
+      </div>
+    )
+  }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
